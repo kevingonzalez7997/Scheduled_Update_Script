4eeee2746de6b4825c741a42539587dd98b6316c
kevingonzalez7997 <59447523+kevingonzalez7997@users.noreply.github.com>
2023-09-26 13:39:28 -0400

    Update update_server_script.sh

--- update_server_script.sh
+++ update_server_script.sh
@@ -6,50 +6,26 @@
 #The amount of upgradeable files will be recorded
 #The recorded amount will then be stored in a file with today's date
 
-
-
-#This command is going to update the server and store results in update_server
-#-y will automatically type yes to any prompt by sudo apt update
-#$() is to be able to run commands inside of variables
-#It is stored in a variable for later use
-update_server=$(sudo apt update -y)
-
-
 #This command will grab today's date and format it into YYYY-MM-DD
-#It will use +update to put the word update before formating
 #It is stored into a variable for later use
 #$() for variable commmands
 file_name_daily=$(date +update_"%F".txt)
 
+# We are able to use pipe to make an oneliner
+# We start by updating the system and using that output as the input for our next command
+# tail will grab the last line of the update output, -n 1 to only focus on the last line as there is the info
+# Now that we have just the last line we only want the first sentence so we CUT it using '.' as what separates it, as 1 to show we just want the first appearance
+# lastly we overwrite the results into the file we created in the previous step
+sudo apt update | tail -n 1 | cut -d '.' -f 1 > $file_name_daily
 
-#First we create a variable, output_from_update
-#Then we run the command echo $(update_server) to get the content of our variable update_server
-#Then we use | pipe to take that output and use it in the awk command
-#awk will scan line by line, we tell it where to look, 'packages'
-#This is based on the output of sudo apt update 
-#The $1,... is to print nth many elements/words in the lines that match with the word packages
-#1-6 are selected so a full sentence can be printed
-output_from_update=$(echo "$update_server" | awk '/packages/{print $1,$2,$3,$4,$5,$6}')
-
-
-#This command will take the line that has been stored in output_from_update
-#It will create a file in our variable file_name_daily and put the content of output_from_update in it
-echo "$output_from_update" > "$file_name_daily"
-
-
-#This will print confirmation that the task is complete
 echo "files have been updated and upgrade information has been stored"
 
-
-
-#Lastly we have to create the corn
-#cron allows us to run tasks automatically on a given time/schedules
-#First we open our crontab 
-#crontab -e
-#Then we add our given schedule 0 23 * * 5
-#0=min 23=hr *=day *=month 5=5th day of the week , Friday
-#Follow that by the path of where the script is 
-#In this case it was in the home directory
-#0 23 * * 5 /~/update_server_script.sh
-
-
+# Lastly we have to create the corn
+# cron allows us to run tasks automatically on a given time/schedules
+# First we open our crontab 
+# crontab -e
+# Then we add our given schedule 0 23 * * 5
+# 0=min 23=hr *=day *=month 5=5th day of the week , Friday
+# Follow that by the path of where the script is 
+# In this case it was in the home directory
+# 0 23 * * 5 /~/update_server_script.sh
